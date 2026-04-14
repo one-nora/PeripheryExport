729d762710e7782ec1b66f3d16cdf2ab68663890
one-nora <193192971+one-nora@users.noreply.github.com>
2026-04-14 15:28:34 -0300

    Copy over plugins that dont need to be decompiled

--- Source/AssetRipper.Export.UnityProjects/Project/CopyBaseProject.cs
+++ Source/AssetRipper.Export.UnityProjects/Project/CopyBaseProject.cs
@@ -1,15 +1,12 @@
 ﻿using AssetRipper.Export.Configuration;
 using AssetRipper.Export.UnityProjects.Configuration;
 using AssetRipper.Import.Logging;
+using AssetRipper.Import.Structure.Assembly.Managers;
 using AssetRipper.Processing;
 using System;
 using System.Collections.Generic;
 using System.IO.Compression;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using AsmResolver.DotNet;
 
 namespace AssetRipper.Export.UnityProjects.Project
 {
@@ -19,46 +16,9 @@ namespace AssetRipper.Export.UnityProjects.Project
 
 		public void DoPostExport(GameData gameData, FullConfiguration settings, FileSystem fileSystem)
 		{
-			List<string> scriptsToKeep = new List<string>()
-			{
-				"Assembly-CSharp",
-				"Assembly-CSharp-firstpass",
-				"ALINE",
-				"andywiecko.BurstTriangulator",
-				"AstarPathfindingProject",
-				"BakeryRuntimeAssembly",
-				"Clipper2Lib",
-				"Coffee.SoftMaskForUGUI",
-				"Coffee.SoftMaskForUGUI.R",
-				"DarkMachineUI",
-				"DOTween",
-				"Drawing",
-				"Facepunch.Steamworks.Win64",
-				"Febucci.Attributes.Runtime",
-				"Febucci.TextAnimator.Runtime",
-				"Febucci.TextAnimator.TMP.Runtime",
-				"PackageTools",
-				"Pathfinding.Ionic.Zip.Reduced",
-				"PrefabBaker",
-				"SeppePeelman.EditorTools.SurfaceAlignTool",
-				"Sirenix.OdinInspector.Attributes",
-				"Sirenix.Serialization.Config",
-				"Sirenix.Serialization",
-				"Sirenix.Utilities",
-				"WFowler1.BspImporter.Runtime",
-				"XNode",
-			};
-
-			string scriptsDirectory = Path.Combine(settings.AssetsPath, "Scripts");
-			if (Directory.Exists(scriptsDirectory))
-			{
-				foreach (string dir in Directory.GetDirectories(scriptsDirectory))
-				{
-					if (!scriptsToKeep.Contains(Path.GetFileName(dir)))
-						Directory.Delete(dir, true);
-				}
-			}
-
+			DeleteScripts(settings);
+			CopyOverPlugins(gameData, settings, fileSystem);
+			
 			string baseProjectPath = RIFT_PROJECT_PATH;
 			if (!File.Exists(baseProjectPath))
 			{
@@ -69,6 +29,74 @@ namespace AssetRipper.Export.UnityProjects.Project
 			using (ZipArchive zip = new ZipArchive(File.Open(baseProjectPath, FileMode.Open, FileAccess.Read), ZipArchiveMode.Read))
 			{
 				zip.ExtractToDirectory(settings.ProjectRootPath, true);
+			}
+		}
+
+		private void DeleteScripts(FullConfiguration settings)
+		{
+			List<string> scriptsToKeep = new List<string>()
+			{
+				"Assembly-CSharp",
+				"Assembly-CSharp-firstpass",
+				"BakeryRuntimeAssembly",
+				"DarkMachineUI",
+				"DOTween",
+				"Febucci.Attributes.Runtime",
+				"Febucci.TextAnimator.Runtime",
+				"Febucci.TextAnimator.TMP.Runtime",
+				"PrefabBaker",
+			};
+			
+			string scriptsDirectory = Path.Combine(settings.AssetsPath, "Scripts");
+            if (Directory.Exists(scriptsDirectory))
+            {
+            	foreach (string dir in Directory.GetDirectories(scriptsDirectory))
+            	{
+            		if (!scriptsToKeep.Contains(Path.GetFileName(dir)))
+            			Directory.Delete(dir, true);
+            	}
+            }
+		}
+
+		private void CopyOverPlugins(GameData gameData, FullConfiguration settings, FileSystem fileSystem)
+		{
+			List<string> pluginsToCopy = new List<string>()
+			{
+				"ALINE",
+				"andywiecko.BurstTriangulator",
+				"Clipper2Lib",
+				"Coffee.SoftMaskForUGUI",
+				"Coffee.SoftMaskForUGUI.R",
+				"DarkMachineUI",
+				"Drawing",
+				"Facepunch.Steamworks.Win64",
+				"Pathfinding.Ionic.Zip.Reduced",
+				"SeppePeelman.EditorTools.SurfaceAlignTool",
+				"Sirenix.OdinInspector.Attributes",
+				"Sirenix.Serialization.Config",
+				"Sirenix.Serialization",
+				"Sirenix.Utilities",
+				"WFowler1.BspImporter.Runtime",
+				"XNode",
+			};
+			
+			IAssemblyManager assemblyManager = gameData.AssemblyManager;
+			AssemblyDefinition[] assemblies = assemblyManager.GetAssemblies().ToArray();
+			if (assemblies.Length != 0)
+			{
+				string pluginsDirectory = fileSystem.Path.Join(settings.AssetsPath, "Plugins");
+				if (!Directory.Exists(pluginsDirectory))
+					Directory.CreateDirectory(pluginsDirectory);
+				
+				foreach (AssemblyDefinition assembly in assemblies)
+				{
+					Logger.Info(LogCategory.Export, $"{assembly.Name}");
+					if (pluginsToCopy.Contains(assembly.Name))
+					{
+						string filepath = fileSystem.Path.Join(pluginsDirectory, SpecialFileNames.AddAssemblyFileExtension(assembly.Name!));
+						assemblyManager.SaveAssembly(assembly, filepath, fileSystem);
+					}
+				}
 			}
 		}
 	}
